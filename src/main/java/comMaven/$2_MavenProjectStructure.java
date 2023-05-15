2e6d084aedfbca2d424298d629efbf44ed65e6c8
混沌天炁 <83585673+hunduntianqi@users.noreply.github.com>
2023-05-15 19:14:15 +0800

    Add files via upload

---
+++ src/main/java/comMaven/$2_MavenProjectStructure.java
@@ -0,0 +1,13 @@
+package comMaven;
+
+/*
+    Maven工程目录结构:
+        $(basedir): 项目根目录, 存放pom.xml和所有子目录
+        $(basedir)/src/main/java: 项目的java源代码
+        $(basedir)/src/main/resources: 存放项目的资源文件, 比如property文件
+        $(basedir)/src/test/java: 存放项目的测试类, 比如JUnit代码
+        $(basedir)/src/test/resources: 存放测试类使用的资源文件
+ */
+
+public class $2_MavenProjectStructure {
+}
